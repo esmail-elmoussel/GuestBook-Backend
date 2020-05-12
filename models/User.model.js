b8b85c74bd8a59b3bcf1b9fa3f867ec4e4d4f379
Esmail <elmoussel12@gmail.com>
2020-05-13 00:20:24 +0200

    create user schema

---
+++ models/User.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: [true, "username is required!"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required!"],
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required!"],
+    minlength: 1,
+    maxlength: 100,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
