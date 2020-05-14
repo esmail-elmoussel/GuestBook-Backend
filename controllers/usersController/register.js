8ea145adfa8bcce2f5d9b7e642f90c6b787b3bda
Esmail <elmoussel12@gmail.com>
2020-05-14 15:58:44 +0200

    edit register user

--- controllers/usersController/register.js
+++ controllers/usersController/register.js
@@ -1,20 +1,24 @@
 const register = (User, bcrypt) => (req, res) => {
   const { username, password } = req.body;
 
-  const hash = bcrypt.hashSync(password);
-  User.findOne({ username })
-    .then((user) => {
-      if (user) {
-        res.status(400).json("Username already exist!");
-      } else {
-        User.create({ username, password: hash }).then((newUser) =>
-          res.json(newUser)
-        );
-      }
-    })
-    .catch(() =>
-      res.status(500).json("an error occurred please try again later")
-    );
+  if (!username.length && !password.length) {
+    res.status(400).json("both username & password are required!");
+  } else {
+    const hash = bcrypt.hashSync(password);
+    User.findOne({ username })
+      .then((user) => {
+        if (user) {
+          res.status(400).json("Username already exist!");
+        } else {
+          User.create({ username, password: hash }).then((newUser) =>
+            res.json(newUser)
+          );
+        }
+      })
+      .catch(() =>
+        res.status(500).json("an error occurred please try again later")
+      );
+  }
 };
 
 module.exports = register;
